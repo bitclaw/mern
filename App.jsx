288a54b5d64232b10a0a3ddcf9b60392755f5a1f
Daniel C <bitclaw@gmail.com>
2017-06-21 04:32:04 -0500

    Separate jsx code

---
+++ App.jsx
@@ -0,0 +1,3 @@
+let contentNode = document.getElementById('contents');
+let component = <h1>Hello World!</h1>;   // A simple JSX component
+ReactDOM.render(component, contentNode);   // Render the component inside the content Node
